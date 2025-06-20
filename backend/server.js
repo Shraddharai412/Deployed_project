04e30275f9e43fa6f08fadbbd27f512059e0ea83
Shraddharai412 <shraddharai412@gmail.com>
2025-06-20 09:55:48 +0530

    Update: fix routes, env files, and frontend app logic

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+
+app.use(cors({
+  origin: 'https://deployed-project-sand.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Import routes
